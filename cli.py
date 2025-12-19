bf76443bf60003fd2c0bd62a7fa52e26e345d7d5
Leonardo Castro <leocastro987@gmail.com>
2025-12-19 10:48:24 -0300

    Adicionei as funcionalidades basicas de um gerenciador de tarefas

--- cli.py
+++ cli.py
@@ -1,5 +1,6 @@
 # Gerenciador de tarefas (CLI)
 # Adicionar, listar, remover e concluir tarefas
+# Verificar e tratar dados inseridos incorretamente
 # Salvar em arquivo .json ou .txt
 # Extras:
     # Filtro por status (pendente/concluída)
@@ -14,17 +15,47 @@ def initApp():
         add()
     elif userInput == 'list':
         listTask()
+    elif userInput == 'remove':
+        removeTask()
+    elif userInput == 'check':
+        checkedTask()
 
 def add():
-    addInfo = input('Write the task: ')
-    taskList.append(f'{len(taskList) + 1} {addInfo}')
-    print('task added')
-    initApp()
+
+    addInfo = input('Write the task: ("exit" for menu) ')
+
+    if addInfo == '' or addInfo == 'exit':
+        listTask()
+    else:
+        taskList.append([f'{addInfo}', False])
+        print('task added\n')
+        add()
 
 def listTask():
-    for task in range(len(taskList)):
-        print(taskList[task])
-        initApp()
+    if len(taskList) == 0:
+        print("To do list empty \nLet's start!")
+    else:
+        for task in range(len(taskList)):
+        
+            if taskList[task][1] == False:
+                print(f'{task + 1}: {taskList[task][0]} [ ]')
+            elif taskList[task][1] == True:
+                print(f'{task + 1}: {taskList[task][0]} [X]')
+            
+    initApp()
+
+def removeTask():
+    removeTaskId = int(input('What is the task index? '))
+    taskList.pop(removeTaskId - 1)
+    print('Done')
+    listTask()
+
+def checkedTask():
+    checkedTaskId = int(input('What is the task index? '))
+    #ternary expression
+    taskList[checkedTaskId - 1][1] = True if taskList[checkedTaskId - 1][1] == False else False 
+    listTask()
+
 
 
 
