ae455f9065081dde12d30dc2256cacf0aefedf94
Leonardo Castro <leocastro987@gmail.com>
2025-12-25 11:10:13 -0300

    criei a função de listar o estoque de carros de forma ordenada ou não. Usei Pathlib para garantir que os arquivos sejam salvos no caminho correto

--- CLI_utilidades/cli_toolkit.py
+++ CLI_utilidades/cli_toolkit.py
@@ -1,12 +1,12 @@
 # CLI de utilidades capaz de executar diferentes tarefas
 # criar um arquivo JSON e salvar na pasta correta
 # Converter JSON para CSV
-# Contar palavras, linhas e caracteres de um arquivo
-# Renomear arquivos em lote
-# Validar CPF/CNPJ
+# listar e ordenar os dados
+# Validar CPF/CNPJ para compra
 
 import json
 import csv
+from pathlib import Path
 
 #Modelos em estoque
 cars = [{'brand': 'Chevrolet', 'model': 'chevette', 'year':1977},
@@ -14,14 +14,16 @@ cars = [{'brand': 'Chevrolet', 'model': 'chevette', 'year':1977},
         {'brand': 'Haval', 'model': 'H6 GT', 'year':2025}]
 
 def initApp():
-    user_input = input('TOOLKIT for car dealership: add, save, csv: ').lower()
+    user_input = input('TOOLKIT for car dealership: add, list, save, csv: ').lower()
 
     if user_input == 'add':
         add_car()
     elif user_input == 'save':
-        save_car()
+        save_cars()
     elif user_input == 'csv':
         convert_csv()
+    elif user_input == 'list':
+        list_cars()
 
 def add_car():
     user_car_Input = input('type separed by commas \',\': brand, model, year: ').lower()
@@ -29,21 +31,27 @@ def add_car():
         initApp()
 
     user_car_Input_arr = user_car_Input.replace(' ', '').split(',')
-    cars.append({'brand': user_car_Input_arr[0], 'model': user_car_Input_arr[1], 'year':user_car_Input_arr[2]})
+    cars.append({'brand': user_car_Input_arr[0], 'model': user_car_Input_arr[1], 'year': int(user_car_Input_arr[2])})
     print('Done!\n')
     print(cars)
     add_car()
 
-def save_car():
-    with open('cars.json', 'w', encoding='utf-8') as file:
+def save_cars():
+    base_dir = Path(__file__).parent
+    file_path = base_dir / 'cars.json'
+    with open( file_path, 'w', encoding='utf-8') as file:
         json.dump(cars, file, ensure_ascii= False, indent= 4)
     print('Saved!')
     initApp()
 
 def convert_csv():
     data = None
+    #ensures the correct file path.
+    base_dir_json = Path(__file__).parent
+    file_path_json = base_dir_json / 'cars.json'
+
     try:
-        with open('cars.json', 'r', encoding='utf-8') as file:
+        with open(file_path_json, 'r', encoding='utf-8') as file:
             data = json.load(file)
             print(data[0].keys())
     except FileNotFoundError:
@@ -52,14 +60,34 @@ def convert_csv():
     if not isinstance(data, list) or not data:
         initApp()
 
+    #ensures the correct file path.
+    base_dir_csv = Path(__file__).parent
+    file_path_csv = base_dir_csv / 'cars.csv'
+    
     try:
-        with open('cars.csv', 'w', newline= '',encoding='utf-8') as csv_file:
+        with open(file_path_csv, 'w', newline= '',encoding='utf-8') as csv_file:
             writer = csv.DictWriter(csv_file, fieldnames= data[0].keys())
             writer.writeheader()
             writer.writerows(data)
         print("CSV file successfully created!")
     except FileNotFoundError:
         print('File not Found')
+    initApp()
+
+def list_cars():
+    user_list_type = input('Sorted or All? ').lower()
+    data = None
+    idx = 1
+
+    if user_list_type == 'all':
+        data = cars        
+    elif user_list_type == 'sorted':
+        data = sorted(cars, key= lambda car: car['year'], reverse=True)
+    
+    for el in data:
+        print(f'{idx} car: {el['model']} by {el['brand']}, year {el['year']}')
+        idx += 1
+    idx = 1
     initApp()
 
 initApp()
