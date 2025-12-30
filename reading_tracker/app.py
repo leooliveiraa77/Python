167c5146346da88df4d56b9c45fa2358cd85d534
Leonardo Castro <leocastro987@gmail.com>
2025-12-30 16:40:58 -0300

    Terminei de implementar todas a features do projeto

--- reading_tracker/app.py
+++ reading_tracker/app.py
@@ -1,10 +1,16 @@
-from database import get_connection, create_tables, add_book, get_all_books, delete_book
+#terminar de emplementar todas as features do CLI OK
+#refatorar o codigo
+#criar uma função SELECT_BY_ID no.db
+#implementar o argpparse
+#finalizar projeto
+
+from database import get_connection, create_tables, add_book, get_all_books, delete_book, get_books_by_status, update_status
 
 MENU_PROMPT = """" -- READING TRACKER --
 Chose one option from menu:
 
 1) Add reading
-2) Add reading status
+2) Updating reading status
 3) List readings
 4) Delete reading
 5) Filter by status
@@ -19,38 +25,80 @@ def init_app():
         if user_input == 1:
             promp_add_book()
         elif user_input == 2:
-            pass
+            promp_update_status()
         elif user_input == 3:
             promp_list_books()
         elif user_input == 4:
             promp_delete_book()
         elif user_input == 5:
-            pass
+            promp_filter_status()
         elif user_input == 6:
             print('bye!')
             break
         else:
             print('Invalid format, try again!')
             continue
-    
-    
 
 def promp_add_book():
     user_reading = input('Add your current reading and information separetaded by commas (ex: title, author, year): ')
-    reading = [item.strip() for item in user_reading.split(',')]
-    print(reading)
-    add_book(get_connection, reading[0], reading[1], int(reading[2]))
-    print('Added!')
+    reading = [item.strip() for item in user_reading.split(',')]    
+    book_status = promp_get_status_handler()
+    add_book(get_connection, reading[0], reading[1], int(reading[2]), book_status)    
+    print(f'{reading[0]} was added!\n')
 
 def promp_list_books():
-    print(get_all_books(get_connection))
+    for el in get_all_books(get_connection):
+        print(f' [ID]: {el[0]} [Titulo]: {el[1]} by {el[2]}, [year]: {el[3]} [STATUS]: {el[4]}')
 
 def promp_delete_book():
     promp_list_books()
-    reading_id = input('Which book do you want to deleteby ID?')
+    reading_id = promp_get_id_handler()
     book = delete_book(get_connection, reading_id)
     print( f'\nDone the {book} is deleted')
+
+def promp_filter_status():
+    FILTER_STATUS = promp_get_status_handler()     
+    filtered_list = get_books_by_status(get_connection, FILTER_STATUS)
+
+    print(f' -- {FILTER_STATUS.upper()} LIST --')
+    for el in filtered_list:
+        print(f'* {el[1]} by {el[2]} : {el[3]}')
+    print ('----------------------------\n')
+
+def promp_get_status_handler():
+    USER_STATUS = input(
+    '''Chose a status number: 
+    1: reading
+    2: want to read
+    3: already read
+    => ''')
+
+    status = None
     
+    if not USER_STATUS.isdigit() or int(USER_STATUS) > 3 or int(USER_STATUS) < 1:
+        print('Invalid entry\n')
+        init_app()
+    elif USER_STATUS == '1':
+        status = 'reading'
+    elif USER_STATUS == '2':
+        status = 'want to read'
+    elif USER_STATUS == '3':
+        status = 'already read'
+
+    return status
+
+def promp_get_id_handler():
+    #Criar validação de dados
+    id = input('Select a book by ID? ')
+    return id 
+def promp_update_status():
+    promp_list_books()
+
+    update_id = promp_get_id_handler()
+    NEW_STATUS = promp_get_status_handler()
+
+    updated_reading = update_status(get_connection, NEW_STATUS, update_id)
+    print(f' {updated_reading} UPDATED!')
 
 init_app()
   
--- reading_tracker/database.py
+++ reading_tracker/database.py
@@ -4,8 +4,8 @@ from pathlib import Path
 CREATE_BOOKS_TABLE = "CREATE TABLE IF NOT EXISTS books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, author TEXT, year INT, status TEXT)"
 INSERT_BOOK = "INSERT INTO books (title, author, year, status) VALUES (?, ?, ?, ?)"
 SELECT_ALL_BOOKS = 'SELECT * FROM books'
-SELECT_BOOK_BY_NAME = 'SELECT * FROM books WHERE title = ?'
-SELECT_BOOK_BY_AUTHOR = 'SELECT * FROM books WHERE author = ?'
+SELECT_BOOK_BY_STATUS = 'SELECT * FROM books WHERE status = ?'
+UPDATE_BOOK_STATUS= 'UPDATE books SET status = ? WHERE id = ?'
 DELETE_BOOK_BY_ID = 'DELETE FROM books WHERE id = ?'
 
 def get_connection():
@@ -21,7 +21,7 @@ def create_tables(connection):
         cursor = conn.cursor()
         cursor.execute(CREATE_BOOKS_TABLE)
     
-def add_book(connection, title, author, year, status = 'r'):
+def add_book(connection, title, author, year, status):
     with connection() as conn:
         cursor = conn.cursor()
         cursor.execute(INSERT_BOOK, (title, author, year, status))
@@ -32,17 +32,26 @@ def get_all_books(connection):
         cursor.execute(SELECT_ALL_BOOKS)
         return cursor.fetchall()
 
-def get_books_by_name(connection, title):
-    with connection() as conn:
-        cursor = conn.cursor()
-        cursor.execute(SELECT_BOOK_BY_NAME, (title))
-        return cursor.fetchall()
-    
 def delete_book(connection, reading_id):
     with connection() as conn:
         cursor = conn.cursor()
-        cursor.execute('SELECT * FROM books WHERE id = ?', reading_id)
+        cursor.execute('SELECT * FROM books WHERE id = ?', (reading_id,))
         book_deleted = cursor.fetchone()
         cursor.execute(DELETE_BOOK_BY_ID, (reading_id))
         return book_deleted
+    
 
+def get_books_by_status(connection, status):
+    with connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute(SELECT_BOOK_BY_STATUS, (status,))
+        filter = cursor.fetchall()
+        return filter
+    
+def update_status(connection, new_status, id):
+    with connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute(UPDATE_BOOK_STATUS, (new_status, id))
+        cursor.execute('SELECT * FROM books WHERE id = ?',(id,))
+        reading_updated = cursor.fetchone()
+        return reading_updated
